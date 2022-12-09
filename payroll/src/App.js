13d388ce23f8ca1762a561c3ea19ef9b4a6e4fd7
nordin <vipnordin@gmail.com>
2022-12-09 22:33:00 +0800

    updated props for basic & ot

--- payroll/src/App.js
+++ payroll/src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import StatutoryTable from './components/StatutoryTable';
 import InputBasic from "./components/InputBasic";
 import Navbar from './components/Navbar'
+import React, {useEffect, useState}from 'react'
 
 function App() {
+  const [basic,setBasic]=useState(0)
+  const [ot,setOt]=useState(0)
+
+  function getData(){
+    console.log('clicked Calculate!!!')
+  }
+
+  useEffect(()=>console.log(`Basic Salary: ${basic}, OT: ${ot}`),[basic,ot])
   return (
     <div className="App">
       <Navbar/>
-      <InputBasic/>
+      <InputBasic salary={setBasic} ot={setOt} calculate={getData}/>
       <StatutoryTable/>
     </div>
   );
--- payroll/src/components/InputBasic.jsx
+++ payroll/src/components/InputBasic.jsx
@@ -1,29 +1,29 @@
 import React ,{Fragment} from 'react'
 
-export default function InputBasic(){
-
+export default function InputBasic(props){
     return(
         <Fragment>
-      <label for="basic">Basic Salary + Fixed Allowance (RM):</label>
+      <label htmlFor="basic">Basic Salary + Fixed Allowance (RM):</label>
       <input
       className='input_basic'
         type="number"
         name="basic_salary"
         id="basic"
-        maxlength="8"
+        maxLength="8"
         placeholder="RM"
-        onChange= {e=>console.log(e.target.value)}
+        onChange= {e=>props.salary(e.target.value)}
       />
-      <label for="ot">Total Overtime (RM):</label>
+      <label htmlFor="ot">Total Overtime (RM):</label>
       <input
       className='input_ot'
         type="number"
         name="ot_amount"
         id="ot"
-        maxlength="8"
+        maxLength="8"
         placeholder="RM"
+        onChange= {e=>props.ot(e.target.value)}
       />
-      <button className="btn_cal" type="submit">Calculate!</button>
+      <button className="btn_cal" type="submit" onClick={()=>props.calculate()}>Calculate!</button>
       </Fragment>
     )
 }
