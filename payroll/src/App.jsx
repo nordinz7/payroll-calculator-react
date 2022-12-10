4f9a9f6a411965c5a335dcdb5a417acbd8094e0b
nordin <vipnordin@gmail.com>
2022-12-10 08:09:40 +0800

    removed usestate and useeffect import in App component

--- payroll/src/App.jsx
+++ payroll/src/App.jsx
@@ -2,21 +2,20 @@ import './App.css';
 import StatutoryTable from './components/StatutoryTable';
 import InputBasic from "./components/InputBasic";
 import Navbar from './components/Navbar'
-import React, {useEffect, useState}from 'react'
+import React from 'react'
 
 function App() {
-  const [basic,setBasic]=useState(0)
-  const [ot,setOt]=useState(0)
+  
 
   function getData(){
     console.log('clicked Calculate!!!')
   }
 
-  useEffect(()=>console.log(`Basic Salary: ${basic}, OT: ${ot}`),[basic,ot])
+  
   return (
     <div className="App">
       <Navbar/>
-      <InputBasic salary={setBasic} ot={setOt} calculate={getData}/>
+      <InputBasic  calculate={getData}/>
       <StatutoryTable/>
     </div>
   );
